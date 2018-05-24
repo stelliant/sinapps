b6689be64c5489f72e1221e8312f94e9b3402c3d
guilloum <guillaume@dev4it.fr>
2018-05-24 17:04:12 +0200

    spring boot

---
+++ src/main/java/eu/stelliant/sinapps/SinappsApplication.java
@@ -0,0 +1,12 @@
+package eu.stelliant.sinapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinappsApplication.class, args);
+	}
+}
