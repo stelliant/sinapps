bc06bf51976710acc58c5fadbefca251d5fb53ae
guilloum <guillaume@dev4it.fr>
2018-06-06 23:31:28 +0200

    new local profile for developers, and test that include local

--- src/main/java/eu/stelliant/sinapps/module/integration/config/IntegrationConfig.java
+++ src/main/java/eu/stelliant/sinapps/module/integration/config/IntegrationConfig.java
@@ -1,9 +1,12 @@
 package eu.stelliant.sinapps.module.integration.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-/*@ImportResource("classpath:integration/api-connexion.xml")*/
+@ImportResource("classpath:integration/api-connexion.xml")
+@Profile("!test")
 public class IntegrationConfig {
 
 }
